9f6128016035c8f5aa365ab546adf0f14978405b
mikeias-silva <mikeias26@gmail.com>
2023-01-11 20:16:34 -0300

    listando feriados

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const apiFeriados = "https://brasilapi.com.br/api/feriados/v1/2022";
+const apiFeriados = "https://brasilapi.com.br/api/feriados/v1/2023";
 
 function feriados() {
 
@@ -9,12 +9,36 @@ function feriados() {
         })
         .then(json => {
             console.log(json);
+            loadPage(json);
         })
         .catch(error => {
             console.log("Erro aqui:", error);
         });
 }
 
-const divContent = document.querySelector('#content');
-console.log(divContent);
+function loadPage(json) {
 
+    const divContent = document.querySelector('#content');
+    // const novoElemento = json.map(element => {
+    //     let elemento = '';
+    //     // console.log(element.name);
+    //     return element.name;
+    // })
+    // console.log(novoElemento);
+
+    json.forEach(element => {
+        let date = element.date;
+        let testeDate = date.split('-');
+        console.log("testedate", testeDate[2]);
+        let newDate = new Date(testeDate[0], testeDate[1], testeDate[2])
+        console.log("date", date);
+        console.log(newDate.getMonth());
+        console.log("newdate", newDate.toLocaleDateString('pt-BR'));
+        // let dataFormatada = newDate.toLocaleDateString('pt-BR')
+        let dataFormatada = testeDate[2] + '/' + testeDate[1] + '/' + testeDate[0];
+        divContent.innerHTML += `<li>dia: ${dataFormatada} ${element.name}</li > `
+    });
+
+}
+
+feriados();
